7c073ea46b1ab5bff17f50362d6518e0ef50dd73
Kristin Salvacion <nitnitks@gmail.com>
2025-02-06 10:51:14 -0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: <Color>[
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 9, 1, 24),
+            ],
+          ),
+        ),
+        child:const Center(
+          child: Text(
+            "Hello World!",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 34,
+            )),
+        ),
+      ),
+    ),
+  ));
+}
