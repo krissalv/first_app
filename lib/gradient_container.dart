b305edbdca803335e398fe3be4af3347d0315e9a
Kristin Salvacion <nitnitks@gmail.com>
2025-02-11 11:45:11 -0500

    Added Images and changed pubsec file

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,22 +2,29 @@ import 'package:first_app/centered.dart';
 import 'package:flutter/material.dart';
 
 class GradientContainer extends StatelessWidget{
-  const GradientContainer({super.key});
+  GradientContainer(this.startAlignment, this.endAlignment,{super.key});
+  Alignment startAlignment; 
+  Alignment endAlignment; 
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: <Color>[
+            begin: startAlignment,
+            end: endAlignment,
+            colors: const [
               Color.fromARGB(255, 26, 2, 80),
               Color.fromARGB(255, 9, 1, 24),
             ],
           ),
         ),
-        child: Centered("Welcome ITEC 315")
+        child: Center(
+          child: Image.asset(
+            'assets/images/dice-2.png',
+            width: 200,
+          ),
+      ),
       );
   }
 
